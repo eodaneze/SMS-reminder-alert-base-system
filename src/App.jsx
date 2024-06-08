15236ddb24bd62f5b41bb0e2445bccdc94763515
eodaneze <ezealiokechukwu1999@gmail.com>
2024-06-08 07:04:52 -0700

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+       <div className="">
+           <h1 className="text-3xl font-bold underline text-center">This is tailwind css</h1>
+       </div>
+    </>
+  )
+}
+
+export default App
